e0f6ec1edce36f2caa1573ccd49903d98804c8e2
jessexu117 <jessexu117@outlook.com>
2017-09-28 11:05:38 +0800

    Add configuration file

---
+++ conf.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+
+OUT_PATH = os.path.join(os.path.expanduser('~'))
+
+LOG = {
+    'ROOT_LEVEL': logging.INFO,
+    'CONSOLE_LEVEL': logging.INFO,
+    'FILE_LEVEL': logging.INFO,
+    'TO_FILE': True
+}
