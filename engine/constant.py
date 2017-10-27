c25f65275a0696219d634666d5c3e209f588b637
jessexu117 <jessexu117@outlook.com>
2017-10-27 16:56:14 +0800

    add constant.py

---
+++ engine/constant.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Constant Collection
+
+@author: Jesse J. Hsu
+@email: jessexu117@outlook.com
+@version: 0.1
+"""
+
+# default empty value
+EMPTY_INT = 0
+EMPTY_FLOAT = 0.0
+EMPTY_STRING = ''
+EMPTY_LIST = []
+
+# signal direction
+SIGNAL_LONG = 'LONG'
+SIGNAL_SHORT = 'SHORT'
+SIGNAL_EXIT = 'EXIT'
+
+# order direction
+ORDER_BUY = 'BUY'
+ORDER_SELL = 'SELL'
+
+# order type
+MARKET_ORDER = 'MKT'
+LIMITED_ORDER = 'LMT'
+
+# order status type
+
+# event type
+EVENT_TICK = 'TICK'
+EVENT_BAR = 'BAR'
+EVENT_SIGNAL = 'SIGNAL'
+EVENT_ORDER = 'ORDER'
+EVENT_FILL = 'FILL'
